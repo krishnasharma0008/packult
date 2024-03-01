5e85824c42ecf531484e66adc4e57c855f55c953
Krishna <starline0008@gmail.com>
2024-03-01 14:43:44 +0530

    #

--- components/videoGallery.js
+++ components/videoGallery.js
@@ -73,164 +73,23 @@ const VideoGallery = () => {
         </div>
       </div>
       <div className={style.videoinfo}>
-        <h3 className={style.videotitle}>{selectedVideo.name}</h3>
-        <p className={style.videodescription}>{selectedVideo.content}</p>
+        <h3>{selectedVideo.name}</h3>
+        <p>{selectedVideo.content}</p>
       </div>
       {/* Updated mobile video list with previous and next buttons */}
       {mobile && (
-      <div className="mobile-video-list">
-        <button className="scroll-button" onClick={() => handleScroll('up')}>◄</button>
-        <div className="thumbnail-container">
+      <div className={style.mobilevideolist}>
+        <button className={style.scrollbutton} onClick={() => handleScroll('up')}>◄</button>
+        <div className={style.thumbnailcontainer}>
           {videoArray.filter((_, index) => index !== selectedVideoIndex).slice(0, visibleVideos).map((video, index) => (
             <div key={index} className={style.videoitem} onClick={() => handleThumbnailClick(index)}>
-              <iframe src={video.url} frameBorder="0" allowFullScreen style={{ width: "100%", height: "97.5%" }}></iframe>
+              <iframe src={video.url} frameBorder="0" allowFullScreen ></iframe>
             </div>
           ))}
         </div>
-        <button className="scroll-button" onClick={() => handleScroll('down')}>►</button>
+        <button className={style.scrollbutton} onClick={() => handleScroll('down')}>►</button>
       </div>
       )}
-      <style jsx>{`
-        .video-and-list-container {
-          display: flex;
-          align-items: center;
-        }
-
-        .gallery-container {
-          display: flex;
-          align-items: center;
-        }
-
-        .video-player {
-          //flex: 1;
-          margin-right: 90px; /* Adjust margin for spacing */
-          width: 80%; /* Updated width */
-        }
-
-        .video-info {
-          margin-top: 10px;
-        }
-
-        .video-title {
-          font-size: 1.5rem;
-          margin-bottom: 0.5rem;
-          color: #000;
-        }
-
-        .video-description {
-          font-size: 1rem;
-          color: #666;
-        }
-
-        .video-player iframe {
-          width: 100%;
-          height: max-content;
-        }
-        
-        .video-list {
-          //flex: 1;
-          display: flex;
-          flex-direction: column;
-          align-items: flex-start;
-          width: 10%; /* Updated width */
-        }
-
-        .thumbnail-container {
-          display: flex;
-          flex-direction: column;
-          //align-items: center;
-          align-items: flex-start; /* Align to the left */
-          overflow-y: auto;  
-          width: 140px; /* Adjusted width */
-          height:auto;        
-        }
-
-        .video-item {
-          margin-top: 20px;
-          cursor: pointer;
-          //height: 133.82px;
-          width: 8.85vw;
-          height:auto;
-          //background-color: #000000;
-        }
-
-        .scroll-button {
-          padding: 5px;
-          cursor: pointer;
-          background-color: #87BE42;
-          color: #fff;
-          border: none;
-          border-radius: 50%;
-          transition: background-color 0.3s ease;
-          margin: 0 auto; /* Center the button */
-          margin-left: 50px;
-        }
-
-        .scroll-button:hover {
-          background-color: #87BE42;
-        }
-
-        .mobile-video-list {
-          display: none;
-        }
-
-        @media (min-width: 768px) and (max-width: 1024px) {
-          .video-player iframe {
-            width: 500px;
-            height: 333px;
-          }
-        }
-        @media (max-width: 767px) {
-          .gallery-container {
-            flex-direction: column;
-          }
-
-          .video-player {
-            margin-right: 0;
-            //margin-bottom: 20px;
-            width: 100%;
-            height: auto;
-          }
-
-          .video-player iframe {
-            width: 100%;
-            height: auto;
-          }
-
-          .video-title {
-            font-family: 'Visby Bold';
-            font-size: 4vw;
-            margin-bottom: 0.5rem;
-            color: #000;
-          }
-
-          .video-description {
-            font-size: 3vw;
-            font-family: 'Visby Regular';
-            color: #666;
-          }
-
-          .video-list {
-            display: none;
-          }
-
-          .mobile-video-list {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-          }
-
-          .mobile-video-list .thumbnail-container {
-            display: flex;
-            overflow-x: auto;
-            margin: 10px 0;
-          }
-
-          .mobile-video-list .scroll-button {
-            display: block;
-          }
-        }
-      `}</style>
     </div>
   );
 };
